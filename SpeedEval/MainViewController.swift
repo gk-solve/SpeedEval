ebf4f7b0a91b0de1ae5586f39c2c02410e4ccdd4
GK <it@soleriant.eu>
2021-05-09 11:18:50 +0100

    Imported CoreGraphics and inserted resultLabel

--- SpeedEval/MainViewController.swift
+++ SpeedEval/MainViewController.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import UIKit
+import CoreGraphics
+
 
 class MainViewController: UIViewController {
 
@@ -15,7 +17,13 @@ class MainViewController: UIViewController {
        
         
         self.view.backgroundColor = UIColor.blue
-    
+        
+        let resultLabel = UILabel()
+        resultLabel.frame = CGRect(x:CGFloat(self.view.frame.size.width/2 - 100), y:50, width:200, height:20)
+        resultLabel.textColor = UIColor.white
+        resultLabel.textAlignment = NSTextAlignment.center
+        resultLabel.text = "Result"
+        self.view.addSubview(resultLabel)
         
     }
 
