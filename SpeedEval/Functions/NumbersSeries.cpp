aae6d78323262b50d70a9e0b457cea103958fa05
GK <it@soleriant.eu>
2021-05-09 11:28:15 +0100

    Inserted C++ file and bridging headers

---
+++ SpeedEval/Functions/NumbersSeries.cpp
@@ -0,0 +1,8 @@
+//
+//  NumbersSeries.cpp
+//  SpeedEval
+//
+//  Created by WESTOWN on 09/05/2021.
+//
+
+#include "NumbersSeries.hpp"
---
+++ SpeedEval/Functions/NumbersSeries.hpp
@@ -0,0 +1,13 @@
+//
+//  NumbersSeries.hpp
+//  SpeedEval
+//
+//  Created by WESTOWN on 09/05/2021.
+//
+
+#ifndef NumbersSeries_hpp
+#define NumbersSeries_hpp
+
+#include <stdio.h>
+
+#endif /* NumbersSeries_hpp */
