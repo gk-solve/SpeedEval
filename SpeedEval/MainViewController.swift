c66a9219a3cae571e837dd5948c3f031d4760f5f
GK <it@soleriant.eu>
2021-05-09 11:24:12 +0100

    UI and comments adjustments

--- SpeedEval/MainViewController.swift
+++ SpeedEval/MainViewController.swift
@@ -15,9 +15,10 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        
+        /* Background color */
         self.view.backgroundColor = UIColor.blue
         
+        /* Result display label */
         let resultLabel = UILabel()
         resultLabel.frame = CGRect(x:CGFloat(self.view.frame.size.width/2 - 100), y:50, width:200, height:20)
         resultLabel.textColor = UIColor.white
